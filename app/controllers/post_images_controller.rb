59db05ff1f62c8e79b1e7a542c92494e4df390ec
EC2 Default User <ec2-user@ip-172-31-1-137.ap-northeast-1.compute.internal>
2022-09-04 13:47:01 +0000

    [Add]ユーザーに関する機能

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -3,7 +3,7 @@ class PostImagesController < ApplicationController
      @post_image =PostImage.new
   end
   
-   def create
+  def create
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
@@ -11,12 +11,17 @@ class PostImagesController < ApplicationController
   end
   
   def index
+    @post_images=PostImage.all
   end
   
   def show
+     @post_image =PostImage.find(params[:id]) 
   end
   
   def destroy
+    @post_image =PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end 
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+ 
 devise_for :users
 get 'homes/about', as: "about"
 root to: "homes#top"
 
-
-
-resources :post_images, only: [:new,:create,:index,:show]
+resources :post_images, only: [:new,:create,:index,:show,:destroy]
+resources :users, only: [:show, :edit, :update]
 end
